72852072808ebe5903da62c9876296deb3a4f1e7
Superbil <superbil@gmail.com>
2017-05-26 07:41:29 +0800

    Let minver emacs version to 25

--- init.el
+++ init.el
@@ -1,12 +1,10 @@
-
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
-(let ((minver "24"))
+(let ((minver "25"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
