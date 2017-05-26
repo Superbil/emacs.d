a155679b573a0bb431fe6487442cc38ed90c709c
Superbil <superbil@gmail.com>
2017-05-27 03:40:02 +0800

    Revert "Remove after-load macro"
    This reverts commit f208d0726985695ed29d90a8eb5533210c99866a.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,3 +1,12 @@
+(if (fboundp 'with-eval-after-load)
+    (defalias 'after-load 'with-eval-after-load)
+  (defmacro after-load (feature &rest body)
+    "After FEATURE is loaded, evaluate BODY."
+    (declare (indent defun))
+    `(eval-after-load ,feature
+       '(progn ,@body))))
+
+
 ;;----------------------------------------------------------------------------
 ;; Handier way to add modes to auto-mode-alist
 ;;----------------------------------------------------------------------------
