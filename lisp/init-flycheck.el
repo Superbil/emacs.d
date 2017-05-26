01cb588daeb0daaa7c7451920c6f72fe186139a5
Superbil <superbil@gmail.com>
2017-05-27 03:59:41 +0800

    Remove flycheck-color-mode-line-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,10 +1,7 @@
 (use-package flycheck
   :config
   (add-hook 'after-init-hook 'global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-  (use-package flycheck-color-mode-line-mode
-    :config
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
 
 (provide 'init-flycheck)
