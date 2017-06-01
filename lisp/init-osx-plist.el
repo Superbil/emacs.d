fc1e21b803cc0c0f771564f9c2d531046c372a17
Superbil <superbil@gmail.com>
2017-06-01 19:56:52 +0800

    Don't auto convert binary plist file

--- lisp/init-osx-plist.el
+++ lisp/init-osx-plist.el
@@ -1,18 +1,5 @@
 ;; Allow editing of binary .plist files.
 (when *is-a-mac*
-  (add-to-list 'jka-compr-compression-info-list
-               ["\\.nib$"
-                "converting text XML to binary plist"
-                "plutil"
-                ("-convert" "binary1" "-o" "-" "-")
-                "converting binary plist to text XML"
-                "plutil"
-                ("-convert" "xml1" "-o" "-" "-")
-                nil nil "bplist"])
-
-  ;;It is necessary to perform an update!
-  (jka-compr-update)
-
   (defun visit-bplist ()
     (interactive)
     (when (string-match "^bplist" (buffer-string))
