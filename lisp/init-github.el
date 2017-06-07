26e3e1c9f3140f42bc4add7fdb6be39730775abf
Superbil <superbil@gmail.com>
2017-06-07 20:52:57 +0800

    Remove some packages
    - remove bug-reference-github
    
    - remove github-issues

--- lisp/init-github.el
+++ lisp/init-github.el
@@ -3,12 +3,6 @@
   :config
   (add-hook 'yagist-list-hook 'sanityinc/no-trailing-whitespace))
 
-(use-package bug-reference-github
-  :after prog-mode
-  :config
-  (add-hook 'prog-mode-hook 'bug-reference-prog-mode))
-
-(use-package github-issues)
 (use-package magit-gh-pulls
   :commands magit-status)
 
