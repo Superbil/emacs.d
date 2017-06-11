685c071a6f8e417f269ab5d69f6aee8325833489
Superbil <superbil@gmail.com>
2017-06-12 04:14:49 +0800

    Remove no use value on exec-path-from-shell

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -4,7 +4,7 @@
   :init
   (setq exec-path-from-shell-check-startup-files nil)
   :config
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+  (dolist (var '("SSH_AUTH_SOCK" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
 
