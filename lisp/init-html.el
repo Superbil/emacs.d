d41b8df46192d4768e459a5eb26e21c8ffd109cc
Superbil <superbil@gmail.com>
2017-06-12 03:57:00 +0800

    Install new package web-mode

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -7,7 +7,15 @@
   (tagedit-add-paredit-like-keybindings)
   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
-;; Note: ERB is configured in init-ruby-mode
+(use-package web-mode
+  :init
+  (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
+  (setq web-mode-ac-sources-alist '(("css" . (ac-source-css-property))
+                                    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-quoting t)
+  :mode ("\\.html?\\'" . web-mode))
 
 
 (provide 'init-html)
