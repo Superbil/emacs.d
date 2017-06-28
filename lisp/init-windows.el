8d77d62b50afe8a389bcbce2c047aed948c4e4bf
Superbil <superbil@gmail.com>
2017-06-28 20:52:40 +0800

    Prefer lexical-binding to lexical-let

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -23,15 +23,14 @@
   ;; When splitting window, show (other-buffer) in the new window
   ;;----------------------------------------------------------------------------
   (defun split-window-func-with-other-buffer (split-function)
-    (lexical-let ((s-f split-function))
-      (lambda (&optional arg)
-        "Split this window and switch to the new window unless ARG is provided."
-        (interactive "P")
-        (funcall s-f)
-        (let ((target-window (next-window)))
-          (set-window-buffer target-window (other-buffer))
-          (unless arg
-            (select-window target-window))))))
+    (lambda (&optional arg)
+      "Split this window and switch to the new window unless ARG is provided."
+      (interactive "P")
+      (funcall split-function)
+      (let ((target-window (next-window)))
+        (set-window-buffer target-window (other-buffer))
+        (unless arg
+          (select-window target-window)))))
 
   (defun sanityinc/toggle-delete-other-windows ()
     "Delete other windows in frame if any, or restore previous window config."
