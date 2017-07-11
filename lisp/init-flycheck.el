e5bf4ff7c4f962502be3390a56c1ec035da61048
Superbil <superbil@gmail.com>
2017-07-11 17:20:29 +0800

    Let flycheck support org-lint

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,7 +1,25 @@
 (use-package flycheck
+  :preface
+  (defun flycheck-org-lint-start (checker callback)
+    (funcall
+     callback 'finished
+     (save-excursion
+       (mapcar
+        (lambda (err)
+          (goto-char (car err))
+          (flycheck-error-new-at
+           (org-current-line) (1+ (current-column))
+           'warning (cadr err) :checker checker))
+        (org-lint-link-to-local-file (org-element-parse-buffer))))))
   :config
   (add-hook 'after-init-hook 'global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+
+  (flycheck-define-generic-checker 'org-lint
+    "Syntax checker for org-lint."
+    :start 'flycheck-org-lint-start
+    :modes '(org-mode))
+  (add-to-list 'flycheck-checkers 'org-lint))
 
 
 (provide 'init-flycheck)
