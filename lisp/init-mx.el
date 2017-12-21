4e5195f4e48099fbb0b507552553bbe87a8db9de
Superbil <superbil@gmail.com>
2017-12-22 02:54:55 +0800

    Change to use amx
    - update ignore file

--- lisp/init-mx.el
+++ lisp/init-mx.el
@@ -1,12 +1,11 @@
-;; Use smex to handle M-x
-(use-package smex
-  :bind ([remap execute-extended-command] . smex)
+;; Use amx to handle M-x
+(use-package amx
   :init
-  (setq-default smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+  (setq-default amx-save-file (expand-file-name ".amx-items" user-emacs-directory))
   :config
   (add-hook 'after-init-hook
             (lambda ()
-              (smex-initialize))))
+              (amx-mode))))
 
 
 (provide 'init-mx)
