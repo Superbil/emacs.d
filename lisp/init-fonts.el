1efc2247fc65d216dfea7b438dbd08ec27b48ecd
Superbil <superbil@gmail.com>
2018-06-23 03:25:14 +0800

    Revert "Use cnfonts to setup custom font"
    This reverts commit 042e7f32994cf8b962586fd5c0f7299d0307665a.

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,6 +1,12 @@
 ;;; Character sets
 
 
+;;; Changing font sizes
+(use-package default-text-scale
+  :bind (("C-M-=" . default-text-scale-increase)
+         ("C-M--" . default-text-scale-decrease)))
+
+
 (defun sanityinc/maybe-adjust-visual-fill-column ()
   "Readjust visual fill column when the global font size is modified.
 This is helpful for writeroom-mode, in particular."
@@ -12,35 +18,5 @@ This is helpful for writeroom-mode, in particular."
 (add-hook 'visual-fill-column-mode-hook
           'sanityinc/maybe-adjust-visual-fill-column)
 
-;;; Setup chinese font and setup font
-(use-package cnfonts
-  :bind (("C-M-=" . cnfonts-increase-fontsize)
-         ("C-M--" . cnfonts-decrease-fontsize))
-  :init
-  (setq cnfonts-use-face-font-rescale t)
-  (let ((fonts
-         '(("Source Code Pro" "Monaco" "Consolas")
-           ("Source Han Sans TC" "Source Han Sans")
-           ("HanaMinB" "SimSun-ExtB" "MingLiU-ExtB" "PMingLiU-ExtB" "MingLiU_HKSCS-ExtB"))))
-    (setq cnfonts-personal-fontnames fonts)
-    (setq cnfonts--custom-set-fontnames fonts))
-  (setq cnfonts--custom-set-fontsizes
-        '((9    10.5 10.5)
-          (10   12.0 12.0)
-          (11.5 15.0 12.5)
-          (12.5 15.0 16.0)
-          (14   16.5 16.5)
-          (15   18.0 18.0)
-          (16   20.0 19.5)
-          (18   22.0 21.0)
-          (20   24.0 24.0)
-          (22   26.0 25.5)
-          (24   28.5 28.5)
-          (26   32.0 31.5)
-          (28   34.0 33.0)
-          (30   36.0 36.0)
-          (32   38.5 39.0)))
-  :config
-  (cnfonts-enable))
 
 (provide 'init-fonts)
