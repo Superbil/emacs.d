68441fd1dddf9e66753ef621fec7d3da479dd444
Superbil <superbil@gmail.com>
2019-08-22 02:29:17 +0800

    Remove tide-format-before-save

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -92,8 +92,7 @@
 (use-package tide
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         (before-save . tide-format-before-save)))
+         (typescript-mode . tide-hl-identifier-mode)))
 
 
 ;; ---------------------------------------------------------------------------
