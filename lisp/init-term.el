8962077cbfed302e28c6f17161da256cdc615b7e
Superbil <superbil@gmail.com>
2020-11-23 00:22:26 +0800

    Change to use multi-term

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -1,9 +1,11 @@
-(use-package sane-term
-  :commands sane-term
-  :bind (("<f10>" . sane-term))
+;;; init-term.el --- Support elisp manually installed in the site-lisp dir -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package multi-term
+  :bind (("<f10>" . multi-term))
   :config
-  ;;; term use zsh
-  (setq-when-file-existed sane-term-shell-command (executable-find "zsh")))
+  (setq-when-file-existed multi-term-program (executable-find "zsh")))
 
 ;; Non-zero values for `line-spacing' can mess up ansi-term and co,
 ;; so we zero it explicitly in those cases.
@@ -14,3 +16,4 @@
 
 
 (provide 'init-term)
+;;; init-term.el ends here
