899414d70933172db7ac22ac7157842558a0b7cc
Superbil <superbil@gmail.com>
2020-11-28 18:57:59 +0800

    Add init-benchmarking
    from
    https://github.com/purcell/emacs.d/commits/master/lisp/init-benchmarking.el a97dc5a

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
