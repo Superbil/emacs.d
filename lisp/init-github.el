e69c76840766603092bb478429ae424b4dbfc625
Superbil <superbil@gmail.com>
2020-12-06 22:29:15 +0800

    Remove yagist

--- lisp/init-github.el
+++ lisp/init-github.el
@@ -3,13 +3,6 @@
 ;;; Code:
 
 (use-package browse-at-remote)
-(use-package yagist
-  :commands yagist-list
-  :init
-  (setq yagist-encrypt-risky-config t
-        yagist-git-config-with-includes t)
-  :config
-  (add-hook 'yagist-list-hook 'sanityinc/no-trailing-whitespace))
 
 (use-package magit-gh-pulls
   :commands magit-status)
