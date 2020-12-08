40e9e30469c66f86b52e6310ed1cd0c3e5335a1a
Superbil <superbil@gmail.com>
2020-12-09 00:24:49 +0800

    Refactoring load exec-path-from-shell with use-package

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -2,11 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package exec-path-from-shell)
-
-(after-load 'exec-path-from-shell
-            (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GOPATH"))
-              (add-to-list 'exec-path-from-shell-variables var)))
+(use-package exec-path-from-shell
+  :after exec-path-from-shell
+  :init
+  (setq exec-path-from-shell-variables
+        '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GOPATH"))
+  :config
+  (exec-path-from-shell-initialize))
 
 ;; Emacs cask seems to depend on the EMACS environment variable being set to the binary path of emacs.
 (setenv "EMACS"
