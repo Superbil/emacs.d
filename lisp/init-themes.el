aea832dccb6424cb78a57d6ed1e90efbcc123baf
Superbil <superbil@gmail.com>
2020-12-08 23:52:13 +0800

    Add custom theme and don't need to prompt at feature

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,7 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Don't prompt to confirm theme safety. This avoids problems with
+;; first-time startup on Emacs > 26.3.
+(setq custom-safe-themes t)
+
 (use-package dracula-theme)
+(use-package color-theme-sanityinc-solarized)
+(use-package color-theme-sanityinc-tomorrow)
 
 
 (provide 'init-themes)
